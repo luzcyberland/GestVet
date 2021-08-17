6a18c4d62b67f7c051116da91902cf1331099934
luzcyberland <frozentorture@gmail.com>
2021-08-17 16:39:55 -0400

    PGV-28 se agrega login

--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,4 @@
-"""config URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
@@ -30,3 +16,4 @@ urlpatterns = [
     path('modificar_rol/<pk>/',login_required(RolUpdate.as_view()), name='modificarrol'),
     path('eliminar_rol/<int:id_rol>/', login_required(eliminar_rol), name='eliminarrol'),   
 ]
+#hello
