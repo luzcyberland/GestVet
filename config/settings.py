05ab90e8c4b6a90cd0dcaf4f204d1a3e00a43831
luzcyberland <frozentorture@gmail.com>
2021-09-01 18:08:09 -0400

    ajustes de front

--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'GestVetDB',
 		'USER': 'postgres',
-		'PASSWORD': 'admin',
+		'PASSWORD': 'postgres',
 		'HOST': 'localhost',
 		'PORT': 5432,
     }
